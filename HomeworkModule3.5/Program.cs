42d2b8417d51555caf4ac78341f6a3fda6446387
mike19952006 <mike19952006@gmail.com>
2018-02-24 00:58:36 +0200

    Выводим числа кратные 5.

--- HomeworkModule3.5/Program.cs
+++ HomeworkModule3.5/Program.cs
@@ -10,6 +10,16 @@ namespace HomeworkModule3._5
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Enter the first number");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the second number");
+            int b = int.Parse(Console.ReadLine());
+
+            for (int i = Math.Min(a, b); i < Math.Max(a,b); i++)
+            {
+                if (i%5 == 0)
+                    Console.WriteLine(i);
+            }
         }
     }
 }
